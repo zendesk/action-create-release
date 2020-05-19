a23c1db6dabe95fc5146b1c26928c13c5aa3083a
Sathish Subramanian <sathishavm@gmail.com>
2020-05-19 02:59:33 -0700

    Added auto incrementing tag version (#1)
    * Auto tag version
    
    * Added semver
    
    * Fix inital release
    
    * Upgrade actions/core
    
    * Added compute tag
    
    * Fix test cases
    
    * Fixed readme
    
    * Fix runs-on

--- src/create-release.js
+++ src/create-release.js
@@ -1,32 +1,139 @@
 const core = require('@actions/core');
 const { GitHub, context } = require('@actions/github');
 
+const { owner, repo } = context.repo;
+const semver = require('semver');
+const process = require('process');
+
+const octokit = new GitHub(process.env.GITHUB_TOKEN);
+const Semantic = {
+  Major: 'major',
+  Minor: 'minor',
+  Patch: 'patch',
+  Premajor: 'premajor',
+  Prerelease: 'prerelease'
+};
+const prerelease = core.getInput('prerelease', { required: false }) === 'true';
+
+// Check string is null
+function isNullString(string) {
+  return !string || string.length === 0 || string === 'null' || string === 'undefined';
+}
+
+// If there is no previous tag, Then the intial tag will be used
+function initialTag(tag) {
+  const suffix = core.getInput('prerelease_suffix');
+  const newTag = prerelease ? `${tag}-${suffix}.0` : tag;
+
+  return newTag;
+}
+
+async function existingTags() {
+  const { data: refs } = await octokit.git.listMatchingRefs({
+    owner,
+    repo,
+    ref: 'tags'
+  });
+  return refs.reverse();
+}
+
+function semanticVersion(tag) {
+  try {
+    const [version, pre] = tag.split('-', 2);
+    const sem = semver.parse(semver.coerce(version));
+
+    if (!isNullString(pre)) {
+      sem.prerelease = semver.prerelease(`0.0.0-${pre}`);
+    }
+
+    return sem;
+  } catch (_) {
+    // semver will return null if it fails to parse, maintain this behavior in our API
+    return null;
+  }
+}
+
+function determinePrereleaseName(semTag) {
+  if (semTag.prerelease.length > 0) {
+    return semTag.prerelease[0];
+  }
+  return core.getInput('prerelease_suffix') || 'beta';
+}
+
+function computeNextSemantic(semTag) {
+  try {
+    const type = core.getInput('auto_increment_type') || Semantic.Patch;
+    const preName = determinePrereleaseName(semTag);
+
+    switch (type) {
+      case Semantic.Major:
+      case Semantic.Minor:
+      case Semantic.Patch:
+      case Semantic.Premajor:
+      case Semantic.Prerelease:
+        return `${semTag.options.tagPrefix}${semver.inc(semTag, type, preName)}`;
+      default:
+        core.setFailed(
+          `Unsupported semantic version type ${type}. Must be one of (${Object.values(Semantic).join(', ')})`
+        );
+    }
+  } catch (error) {
+    core.setFailed(`Failed to compute next semantic tag: ${error}`);
+  }
+  return null;
+}
+
+async function computeLastTag() {
+  const recentTags = await existingTags();
+  if (recentTags.length < 1) {
+    return null;
+  }
+  return recentTags.shift().ref.replace('refs/tags/', '');
+}
+
+async function computeNextTag() {
+  const lastTag = await computeLastTag();
+
+  // Handle zero-state where no tags exist for the repo
+  if (!lastTag) {
+    return initialTag('v1.0.0');
+  }
+
+  core.info(`Computing the next tag based on: ${lastTag}`);
+  core.setOutput('previous_tag', lastTag);
+
+  const semTag = semanticVersion(lastTag);
+
+  if (semTag == null) {
+    core.setFailed(`Failed to parse tag: ${lastTag}`);
+    return null;
+  }
+  semTag.options.tagPrefix = lastTag.startsWith('v') ? 'v' : '';
+
+  return computeNextSemantic(semTag);
+}
+
 async function run() {
   try {
-    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
-
-    // Get owner and repo from context of payload that triggered the action
-    const { owner, repo } = context.repo;
-
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    const tagName = core.getInput('tag_name', { required: true });
+    const tagName = core.getInput('tag_name', { required: false });
+    // Use predefined tag or calculate automatic next tag
+    const tag = isNullString(tagName) ? await computeNextTag() : tagName.replace('refs/tags/', '');
 
-    // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
-    const tag = tagName.replace('refs/tags/', '');
-    const releaseName = core.getInput('release_name', { required: true }).replace('refs/tags/', '');
+    const releaseName = core.getInput('release_name', { required: false });
+    const release = isNullString(releaseName) ? tag : releaseName.replace('refs/tags/', '');
+
     const body = core.getInput('body', { required: false });
     const draft = core.getInput('draft', { required: false }) === 'true';
-    const prerelease = core.getInput('prerelease', { required: false }) === 'true';
 
     // Create a release
     // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
     // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-    const createReleaseResponse = await github.repos.createRelease({
+    const createReleaseResponse = await octokit.repos.createRelease({
       owner,
       repo,
       tag_name: tag,
-      name: releaseName,
+      name: release,
       body,
       draft,
       prerelease
@@ -38,6 +145,7 @@ async function run() {
     } = createReleaseResponse;
 
     // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    core.setOutput('current_tag', tag);
     core.setOutput('id', releaseId);
     core.setOutput('html_url', htmlUrl);
     core.setOutput('upload_url', uploadUrl);
